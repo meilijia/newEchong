6d92537fd6506db36970b0eaf823b0facfbd5ad1
DingWDong <654216343@qq.com>
2018-02-01 12:40:28 +0800

    change type

--- src/pages/classify/type.vue
+++ src/pages/classify/type.vue
@@ -10,7 +10,7 @@
       <div class="msg-list">
         <div>
           <ul>
-             <li v-for="item in mlist"  v-html="item" :key="item.id"></li>
+             <li v-for="item in mlist" :data-id="item.cateid" v-html="item.name"  @click="setId(item.cateid)" :key="item.id"></li>
           </ul>
         </div>
       </div>
@@ -22,6 +22,18 @@
               <ul class="hot-msg" >
                 <li v-for="item in imgUrl" :key="item.name" @click="goList()">
                   <img :src="item.photo" alt="" />
+                  <p v-html="item.name"></p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- 品牌 -->
+          <div class="hot2">
+            <div class="hot3">
+              <h3  v-html="btit"></h3>
+              <ul class="hot-msg brand" >
+                <li v-for="item in brand" :key="item.name"  @click="goList()">
+                  <img :src="item.logo" alt="" />
                   <p v-html="item.name"></p>
                 </li>
               </ul>
@@ -40,26 +52,61 @@ export default {
   name: 'type',
   data () {
     return {
-      mlist: [
-        '为您推荐 ', ' 狗狗主粮 ', ' 狗狗零食 ', ' 狗狗服饰 ', ' 狗狗窝垫 ', '狗狗生活', '狗狗玩具', '狗狗保健', '狗狗医疗', '狗狗牵引', '狗狗美容', '狗狗周边', '狗狗清洁'
-      ],
+      mlist: [],
       mtit: '',
-      imgUrl: []
+      imgUrl: [],
+      brand: [],
+      btit: '',
+      goodsId: [],
+      id: 88888
     }
   },
   mounted: function () {
-    axios.get('/v3/goods/category/main.html?do=getChildren&owner=88888&pet_type=dog&system=wap&isWeb=1&version=303&_=1517220938674')
+    // 侧边导航的数据
+    axios.get('/v3/goods/category/main.html?pet_type=dog&system=wap&isWeb=1&version=303&_=1517388771715')
       .then((res) => {
-        // console.log(res.data.cate_list[0])
-        this.mtit = res.data.cate_list[0].title
-        for (let i in res.data.cate_list[0].list) {
-          this.imgUrl = this.imgUrl.concat(res.data.cate_list[0].list[i])
-          // console.log(this.imgUrl)
+      //  console.log(res.data.categorys)
+        this.mlist = res.data.categorys
+        for (let i in res.data.categorys) {
+          this.goodsId = this.goodsId.concat(Number(res.data.categorys[i].cateid))
         }
-        // console.log(this.imgUrl)
+        // console.log(this.goodsId)
       })
+    this.load()
   },
   methods: {
+    setId: function (a) {
+      this.id = a
+      this.delete()
+      this.load(this.id)
+    },
+    delete: function () {
+      this.imgUrl.splice(0, 100)
+      this.brand.splice(0, 100)
+    },
+    load: function () {
+      // 右边内容部分
+      axios.get(`/v3/goods/category/main.html?do=getChildren&owner=${this.id}&pet_type=dog&system=wap&isWeb=1&version=303&_=1517220938674`)
+        .then((res) => {
+          // console.log(res.data.cate_list[1])
+          // console.log(res.data.cate_list[0])
+          this.mtit = res.data.cate_list[0].title
+          for (let i in res.data.cate_list[0].list) {
+            this.imgUrl = this.imgUrl.concat(res.data.cate_list[0].list[i])
+          // console.log(this.imgUrl)
+          }
+          if (res.data.cate_list[1]) {
+            this.btit = res.data.cate_list[1].title
+            for (let i in res.data.cate_list[1].list) {
+              this.brand = this.brand.concat(res.data.cate_list[1].list[i])
+              // console.log(this.brand)
+            }
+          } else {
+            this.btit = ''
+          }
+        // console.log(this.imgUrl)
+        })
+    },
     goList: function () {
       axios.get('/v3/goods/list/main.html?version=355&brandid=0&page=1&orderby=def_desc&cateid=413&pet_type=dog&extend_pam=&real_wid=&region=&system=wap&isWeb=1&_=1517300952132')
         .then((res) => {
--- src/pages/my/login.vue
+++ src/pages/my/login.vue
@@ -13,8 +13,8 @@
         </div>
         <div class="ll">
           <ul>
-            <li><a href="javascript:;">普通登录</a><i class="triangle"></i></li>
-            <li><a href="javascript:;">手机动态密码登录</a></li>
+            <li @click="changeMethods1()"><a href="javascript:;">普通登录</a><i class="triangle" v-show="user"></i></li>
+            <li @click="changeMethods2()"><a href="javascript:;">手机动态密码登录</a><i class="triangle" v-show="tel"></i></li>
           </ul>
         </div>
       </div>
@@ -22,7 +22,7 @@
     <!--用户名密码-->
     <div class="profession">
       <div class="formbox">
-        <form action="" method="post">
+        <form action="" method="post" class="user" v-show="user">
           <div>
             <ul class="mform">
               <li>
@@ -40,7 +40,23 @@
             </ul>
           </div>
         </form>
-        <form action="" method="post">
+        <form action="" method="post" class="tel" v-show="tel">
+          <div>
+            <ul class="mform">
+              <li>
+                <span class="iconfont icf">&#xe650;</span>
+                <input type="text" id="username" placeholder="已注册手机号" />
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul class="mform">
+              <li>
+                <span class="iconfont icf">&#xe61b;</span>
+                <input type="text" id="psw" placeholder="输入手机动态密码" />
+              </li>
+            </ul>
+          </div>
         </form>
       </div>
       <!--忘记密码-->
@@ -69,171 +85,23 @@ export default {
   name: 'login',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      user: true,
+      tel: false
+    }
+  },
+  methods: {
+    changeMethods1: function () {
+      this.user = true
+      this.tel = false
+    },
+    changeMethods2: function () {
+      this.user = false
+      this.tel = true
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-  .box
-    background: url(../../assets/img/log.png);
-    background-size: 100% 100%
-
-  .top
-    height: 1rem;
-    line-height: 1rem;
-    position: relative;
-    padding: 0 1em;
-
-  .top>a
-    display: block;
-    width: 22px;
-    height: 20px;
-    position: absolute;
-    margin-top: 16px;
-    background: url(../../assets/img/lt.png) no-repeat;
-    background-size: 12px 17px;
-
-  .top .res
-    right: 10px;
-    position: absolute;
-    top: 0;
-
-  .res>a
-    color: #fff;
-
-  .logo
-    padding: 1em 0 2em 0;
-
-  .logo img
-    display: block;
-    margin: auto;
-    width: 20%;
-
-  .ll
-    background-color: rgba(255,255,255,0.3);
-
-  .ll ul::before
-    display: table;
-    content: " ";
-    clear: both;
-
-  .ll ul::after
-    display: table;
-    content: " ";
-    clear: both;
-
-  .ll ul li
-    float: left;
-    width: 50%;
-    text-align: center;
-    height: 44px;
-    line-height: 44px;
-    position: relative;
-
-  .ll ul li a
-    font-size: 15px;
-    color: #fff;
-    display: block;
-
-  .triangle
-    width: 0;
-    height: 0;
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-bottom: 10px solid #fff;
-    position: absolute;
-    bottom: 0px;
-    left: 0;
-    right: 0;
-    margin: auto;
-    display: block;
-
-  .profession
-    background: white;
-
-  .formbox
-    background: #fff;
-    border-radius: 0.5em;
-    margin: 0em 4% 1em 4%;
-    padding: 0 1.6%;
-
-  .mform li
-    border-bottom: #e2e2e2 solid 1px;
-    padding: 12px 0 12px 30px;
-    position: relative;
-
-  .icf
-    float: left;
-    display: inline;
-    width: 17px;
-    height: 21px;
-    margin: 3px 0 0 -25px;
-    color: #666;
-
-  .mform li input
-    display: block;
-    height: 21px;
-    width: 100%;
-    font-size: 15px;
-    border: none;
-    padding: 0px;
-    color: #999;
-    font-family: "Microsoft Yahei",tahoma,arial;
-    outline: none;
-
-  .forgetpsw
-    padding-left: 2em;
-    padding-right: 2em;
-    font-size: 0.2rem;
-
-  .forgetpsw a
-    color: #898989;
-    font-size: 1.4em;
-    float: right;
-
-  .loginbut
-    padding-top: 0.5em;
-    text-align: center;
-
-  .loginbut a
-    display: block;
-    letter-spacing: 5px;
-    margin: 0 4%;
-    border-radius: 5px;
-    background: #2ec975;
-    font-size: 15px;
-    padding: .6em 0;
-    text-align: center;
-    color: #fff;
-
-  .joinweb
-    color: #d7d7d7;
-    margin-top: 0.5em;
-    font-size: 1em;
-    text-align: center;
-
-  .web
-    padding: 1.5em 0;
-    display: table;
-    content: " ";
-
-  .web::after
-    clear: both;
-
-  .web li
-    width: 33.3%;
-    padding: 0 1.2em;
-    float: left;
-    display: inline;
-
-  .web li img
-    width: 60%;
-    height: auto;
-    display: block;
-    margin: auto;
-    max-width: 176px;
-
+<style scoped lang="stylus" src="../../assets/css/login.css">
 </style>
