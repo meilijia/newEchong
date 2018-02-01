be4b77f1827a861c3e6c4c65e62d18bd5840ab77
meilijia <1056253401@qq.com>
2018-02-01 12:28:30 +0800

    sale

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,16 +1,16 @@
 <template>
   <ul class="footer">
     <li class="footer-element">
-      <router-link to="/"><span class="footer-img index-img" @click="changebg()"></span></router-link>
+      <router-link to="/"><span class="footer-img index-img"></span></router-link>
     </li>
     <li class="footer-element">
-      <router-link to="/type"><span class="footer-img type-img" @click="changebg()"></span></router-link>
+      <router-link to="/type"><span class="footer-img type-img"></span></router-link>
     </li>
     <li class="footer-element">
-      <router-link to="/list"><span class="footer-img list-img" @click="changebg()"></span></router-link>
+      <router-link to="/list"><span class="footer-img list-img"></span></router-link>
     </li>
     <li class="footer-element">
-      <router-link to="/login"><span class="footer-img login-img" @click="changebg()"></span></router-link>
+      <router-link to="/login"><span class="footer-img login-img"></span></router-link>
     </li>
   </ul>
 </template>
